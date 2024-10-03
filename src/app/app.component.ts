2098ceedc256715beb33c0efb4f380d36bde2501
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-10-03 23:10:12 +0530

    Landing Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my-first-project';
+  hoveredIcon: string | null = null; // Track the currently hovered icon
+
+  setHover(icon: string) {
+    this.hoveredIcon = icon;
+  }
+
+  clearHover() {
+    this.hoveredIcon = null;
+  }
 }
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DevallyComponent } from './devally.component';
+import { HomeComponent } from './home.component';
 
-describe('DevallyComponent', () => {
-  let component: DevallyComponent;
-  let fixture: ComponentFixture<DevallyComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DevallyComponent]
+      imports: [HomeComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(DevallyComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
