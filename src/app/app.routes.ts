bb8439320c0aaff734d94e50a37259c99c347bb5
niharika sahu <121661609+nihu-13@users.noreply.github.com>
2024-10-19 01:34:02 +0530

    Team Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,15 @@ import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { ContactComponent } from './contact/contact.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { TeamComponent } from './team/team.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', component: LandingPageComponent },
     {path:'about',component:AboutComponent},
     { path:'services',component:ServicesComponent},
-    { path:'contact',component:ContactComponent}
+    { path:'contact',component:ContactComponent},
+    { path:'team',component:TeamComponent}
 ];
 
 @NgModule({
